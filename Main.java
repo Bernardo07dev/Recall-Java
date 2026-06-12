76eb4eff857213ceffa4bb3fabf17036e13c04d7
Bernardo <bernardomoreira.dev@gmail.com>
2026-06-11 21:06:21 -0300

    Files

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Olá, Mundo!");
+        tente();
+    }
+
+    public static void tente(){
+        System.out.println("Rodar");
+    }
+}
